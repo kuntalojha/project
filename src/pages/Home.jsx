c88b19f28c103c38d3487d69f16bb1eb3d4d341a
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-09-27 21:51:51 +0530

    add axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  axios
+    .get('https://dummyjson.com/products')
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+      console.log(response.data.products);
+      console.log(response.data.products[0]);
+      console.log(response.data.products[1].title);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+
   return (
     <div className='h-screen'>
       <h1>hello from Home.jsx</h1>
     </div>
   );
-}
+};
 
 export default Home;
