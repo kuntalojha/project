6da147f8d73b8f8801b582de9f1bed33c467317e
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-09-27 19:29:47 +0530

    Setup shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1>hello from App.jsx</h1>
     </div>
   );
 }
